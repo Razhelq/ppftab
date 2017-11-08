72baaada2a5849e8d2bc149d3ef2e69fdfd2cd44
Mateusz <33294217+Razhelq@users.noreply.github.com>
2017-11-08 10:58:38 +0100

    Update 5.1.py

---
+++ Python dla Kazdego/Chapter 5/5.1.py
@@ -0,0 +1,19 @@
+#Write ou all given words in random order without repetirions
+
+import random
+
+words = ['starcraft', 'diablo', 'overwatch', 'heartstone', 'witcher']
+new_words = []
+
+count_words = len(words)
+written = 0
+
+while written != count_words:
+    word = random.choice(words)
+    if word not in new_words:
+        new_words.append(word)
+        written += 1
+        print(word)
+
+
+
