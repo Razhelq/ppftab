cfb3940a70bcd8536adc51ca0dd089455e6d05ed
Razhelq <mateuszszpakowski@wp.pl>
2017-11-25 18:15:27 +0100

    Chapter 9 finally finished!

--- PYTHON PROGRAMMING FOR ABSOLUTE BEGINNER/Chapter 9 - OOP/9.4.py
+++ PYTHON PROGRAMMING FOR ABSOLUTE BEGINNER/Chapter 9 - OOP/9.4.py
@@ -9,111 +9,123 @@ class player(object):
     def __init__(self, name):
         self.name = name
 
+    def __str__(self):
+        rep = ""
+        rep += self.name
+        return rep
+
 class Forest(player):
     def choice(self):
         print("You entered mysterious Forest", self.name, "from the distance you can see small cabine, \
 dark cave and dirty lake")
         yesno = gry.ask_yes_no("Would you like to discover this place or step back? (t for Yes / n for No)")
-        choice = "back"
+        choice = ""
         if yesno == "t":
             choice = input("Where do you want to go (cabine / cave / lake)").lower()
             while choice not in ("cabine", "cave", "lake"):
                 choice = input("Where do you want to go (cabine / cave / lake)").lower()
             return choice
         else:
+            choice = "back"
             return choice
         
     def cabine(self):
         print("It is dark and scary place. Something bad happened here...")
         yesno = gry.ask_yes_no("Would you like to check different places in the forest or step back? \
 (t for Yes / n for No)")
-        choice = "back"
+        choice = ""
         if yesno == "t":
             choice = input("Where do you want to go (cave / lake)").lower()
             while choice not in ("cave", "lake"):
                 choice = input("Where do you want to go (cave / lake)").lower()
             return choice
         else:
+            choice = "back"
             return choice
         
     def cave(self):
         print("There are a lot of rats and spiders in here...")
         yesno = gry.ask_yes_no("Would you like to check different places in the forest or step back? \
 (t for Yes / n for No)")
-        choice = "back"
+        choice = ""
         if yesno == "t":
             choice = input("Where do you want to go (cabine / lake)").lower()
             while choice not in ("cabine", "lake"):
                 choice = input("Where do you want to go (cabine / lake)").lower()
             return choice
         else:
+            choice = "back"
             return choice
         
     def lake(self):
         print("You can notice few dead fishes in the dirty water...")
         yesno = gry.ask_yes_no("Would you like to check different places in the forest or step back? \
 (t for Yes / n for No)")
-        choice = "back"
+        choice = ""
         if yesno == "t":
             choice = input("Where do you want to go (cave / cabine)").lower()
             while choice not in ("cave", "lake"):
                 choice = input("Where do you want to go (cave / cabine)").lower()
             return choice
         else:
-            return choice        
-
+            choice = "back"
+            return choice
 
 class Desert(player):
     def choice(self):
         print("You entered big desert", self.name, "from the distance you can see Oaza, \
 deadly tomb and large piramid")
         yesno = gry.ask_yes_no("Would you like to discover this place or step back? (t for Yes / n for No)")
-        choice = "back"
+        choice = ""
         if yesno == "t":
             choice = input("Where do you want to go (oaza / tomb / piramid)").lower()
             while choice not in ("oaza", "tomb", "piramid"):
                 choice = input("Where do you want to go (oaza / tomb / piramid)").lower()
             return choice
         else:
+            choice = "back"
             return choice
 
     def oaza(self):
         print("Finally you can rest, drink water and eat fruity fruit")
         yesno = gry.ask_yes_no("Would you like to check different places in the desert or step back? \
 (t for Yes / n for No)")
-        choice = "back"
+        choice = ""
         if yesno == "t":
             choice = input("Where do you want to go (tomb / piramid)").lower()
             while choice not in ("tomb", "piramid"):
                 choice = input("Where do you want to go (tomb / piramid)").lower()
             return choice
         else:
+            choice = "back"
             return choice
         
     def tomb(self):
         print("Someone died in here...")
         yesno = gry.ask_yes_no("Would you like to check different places in the desert or step back? \
 (t for Yes / n for No)")
-        choice = "back"
+        choice = ""
         if yesno == "t":
             choice = input("Where do you want to go (oaza / piramid)").lower()
             while choice not in ("oaza", "piramid"):
                 choice = input("Where do you want to go (oaza / piramid)").lower()
             return choice
         else:
+            choice = "back"
             return choice
 
     def piramid(self):
         print("Another graveyard, let's better go outside.")
         yesno = gry.ask_yes_no("Would you like to check different places in the desert or step back? \
 (t for Yes / n for No)")
-        choice = "back"
+        choice = ""
         if yesno == "t":
             choice = input("Where do you want to go (oaza / tomb)").lower()
             while choice not in ("oaza", "tomb"):
                 choice = input("Where do you want to go (oaza / tomb)").lower()
             return choice
         else:
+            choice = "back"
             return choice
         
 class Mountains(player):
@@ -121,52 +133,56 @@ class Mountains(player):
         print("You entered big mountains.", self.name, "from the distance you can see really high peak, \
 mountain lake and big cliff")
         yesno = gry.ask_yes_no("Would you like to discover this place or step back? (t for Yes / n for No)")
-        choice = "back"
+        choice = ""
         if yesno == "t":
             choice = input("Where do you want to go (peak / lake / cliff)").lower()
             while choice not in ("peak / lake / cliff"):
                 choice = input("Where do you want to go (peak / lake / cliff)").lower()
             return choice
         else:
+            choice = "back"
             return choice
         
     def peak(self):
         print("The view is really nice, you can see the enviroment is very peacefull.")
         yesno = gry.ask_yes_no("Would you like to check different places in the mountains or step back? \
 (t for Yes / n for No)")
-        choice = "back"
+        choice = ""
         if yesno == "t":
             choice = input("Where do you want to go (lake / cliff)").lower()
             while choice not in ("cave", "lake"):
                 choice = input("Where do you want to go (lake / cliff)").lower()
             return choice
         else:
+            choice = "back"
             return choice
 
     def lake(self):
         print("Clear and blue water reminds you your childhood and fishing with your grandpa.")
         yesno = gry.ask_yes_no("Would you like to check different places in the mountains or step back? \
 (t for Yes / n for No)")
-        choice = "back"
+        choice = ""
         if yesno == "t":
             choice = input("Where do you want to go (peak / cliff)").lower()
             while choice not in ("cave", "lake"):
                 choice = input("Where do you want to go (peak / cliff)").lower()
             return choice
         else:
+            choice = "back"
             return choice
 
     def cliff(self):
         print("Watch out, if you would fall down here, our mission would be finished.")
         yesno = gry.ask_yes_no("Would you like to check different places in the mountains or step back? \
 (t for Yes / n for No)")
-        choice = "back"
+        choice = ""
         if yesno == "t":
             choice = input("Where do you want to go (peak / lake)").lower()
             while choice not in ("cave", "lake"):
                 choice = input("Where do you want to go (peak / lake)").lower()
             return choice
         else:
+            choice = "back"
             return choice
 
 class crossroad(object):
@@ -175,16 +191,14 @@ class crossroad(object):
         desert = Desert(player)
         mountains = Mountains(player)
 
-        finish = gry.ask_yes_no("Would you like to finish the game? \
-    (t for Yes / n for No)")
+        finish = gry.ask_yes_no("Would you like to finish the game? (t for Yes / n for No)")
         if finish == "t":
             exit
         else:
-            choice = ""
-            decision = ""
-            
+            decision = ""            
             while decision not in ("desert", "forest", "mountains"):
                 decision = input("Where would you like to go? (desert / forest / mountains)")
+                choice = ""
                 while choice != "back":
                     choice = ""
                     if decision == "desert":
@@ -219,7 +233,11 @@ class crossroad(object):
                             decision == ""
                         elif choice == "lake":
                             mountains.lake()
-                            decision == ""
+                decision = ""
+                finish = gry.ask_yes_no("Would you like to finish the game? (t for Yes / n for No)")
+                if finish == "y":
+                    exit
+                    
 
         
 class game(object):
