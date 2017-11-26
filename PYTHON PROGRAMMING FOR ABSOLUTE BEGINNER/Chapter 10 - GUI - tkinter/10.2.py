cc05ddfe574f7cea7bbd6faf52ff7e04040599f3
Razhelq <mateuszszpakowski@wp.pl>
2017-11-26 17:22:12 +0100

    10.2 just began

---
+++ PYTHON PROGRAMMING FOR ABSOLUTE BEGINNER/Chapter 10 - GUI - tkinter/10.2.py
@@ -0,0 +1,15 @@
+# Computer choose random number in range(1,100)
+# Player tries to guess the number and app informs him when the number is too small or too big.
+# GUI based app
+
+import random
+from tkinter import *
+
+class Whats_the_number(Frame):
+    def __init__(self,master):
+        super(Whats_the_number, self).__init__(master)
+        self.grid
+        self.create_widgets()
+
+    def create_widgets(self):
+        
