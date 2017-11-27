407848015e91c50451db5a9044c59bcf68463a48
Razhelq <mateuszszpakowski@wp.pl>
2017-11-27 20:39:41 +0100

    10.2 done

--- PYTHON PROGRAMMING FOR ABSOLUTE BEGINNER/Chapter 10 - GUI - tkinter/10.2.py
+++ PYTHON PROGRAMMING FOR ABSOLUTE BEGINNER/Chapter 10 - GUI - tkinter/10.2.py
@@ -2,8 +2,10 @@
 # Player tries to guess the number and app informs him when the number is too small or too big.
 # GUI based app
 
+
 import random
 from tkinter import *
+
 
 class Whats_the_number(Frame):
     def __init__(self, master):
