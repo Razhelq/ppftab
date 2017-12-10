bec0566eb832763072513e93629d0fd271a1f63a
Razhelq <mateuszszpakowski@wp.pl>
2017-12-10 11:08:54 +0100

    cleaning

--- PYTHON PROGRAMMING FOR ABSOLUTE BEGINNER/Chapter 5 - List & Dictionaries/test.py
+++
@@ -1,5 +0,0 @@
-choices = ['strengh', 'wisdom', 'health', 'flex']
-choice = int(input("\n\nWhat is your choice? ")) - 1
-if choice < 4  or choice >= 0:
-    attrib = choices[choice]
-    print(attrib)
